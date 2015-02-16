6ceece7fc917c3bae95dcfcf7a4b4f78180b9651
smcelroy <sdmcelro@asu.edu>
2015-02-16 12:41:56 -0700

    fixed merge with local master

--- src/main/java/edu/asu/cst316/gameboard/GameBoard.java
+++ src/main/java/edu/asu/cst316/gameboard/GameBoard.java
@@ -1,13 +1,10 @@
 package edu.asu.cst316;
 
 public class GameBoard {
-
 	static class GameSpace {
 		String type = "";
 		int size = (Integer) null;
 		GameSpace nextSpace = null;
 		GameSpace alternativeSpace = null;
 	};
-	
-	
 }
