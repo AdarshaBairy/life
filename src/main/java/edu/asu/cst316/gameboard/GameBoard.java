722ee8bf0fadf845489460f04a8a0f30d24c9dbf
smcelroy <sdmcelro@asu.edu>
2015-02-23 10:31:44 -0700

    don't remember what i changed

--- src/main/java/edu/asu/cst316/gameboard/GameBoard.java
+++ src/main/java/edu/asu/cst316/gameboard/GameBoard.java
@@ -4,10 +4,5 @@ public class GameBoard {
 	
 	private static GameSpace playerSpace;
 	
-	public static void createGameBoard(){
-		playerSpace = new GameSpace("start", 3, null, null);
-		playerSpace.setNextSpace(new GameSpace("neutral", 2, null, null));
-		
-	}
 	
 }
