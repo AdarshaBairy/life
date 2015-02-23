6194c9bd1dabb59c903f327a41e8e716e2aba074
smcelroy <sdmcelro@asu.edu>
2015-02-23 11:24:38 -0700

    gameboard progress

--- src/main/java/edu/asu/cst316/gameboard/GameBoard.java
+++ src/main/java/edu/asu/cst316/gameboard/GameBoard.java
@@ -1,8 +1,45 @@
-package main.java.edu.asu.cst316.gameboard;
+package edu.asu.cst316.gameboard;
 
 public class GameBoard {
 	
 	private static GameSpace playerSpace;
+	private static GameSpace endSpace;
 	
+	private static int endX;
+	private static int endY;
+	
+	public GameBoard(){
+		
+		endSpace = new GameSpace("end", 2, null, null, endX, endY);
+		//short red path
+		GameSpace[] redSpaces5 = new GameSpace[7];
+		redSpaces5[6] = new GameSpace("red", 2, endSpace, null, endX-128, endY);
+		
+		int lastX = endX-128;
+		int lastY = endY;
+		
+		for(int i = 5; i >= 0; --i){
+			redSpaces5[i] = new GameSpace("red", 2, redSpaces5[i+1], null, lastX-128, lastY);
+		}
+		
+	
+		//long greenpath
+		GameSpace[] greenSpaces5 = new GameSpace[17];
+		greenSpaces5[16] = new GameSpace("green", 2, endSpace, null, endX, endY-128);
+		
+		lastX = endX;
+		lastY = endY - 128;
+		
+		for(int i = 15; i >= 13; --i){
+			redSpaces5[i] = new GameSpace("green", 2, redSpaces5[i+1], null, endX-128, endY);
+		}
+		greenSpaces5[12] = new GameSpace("green", 2, redSpaces5[13], null, endX, endY-64);
+		for(int i = 11; i >= 7; --i){
+			redSpaces5[i] = new GameSpace("green", 2, redSpaces5[i+1], null, endX-128, endY);
+		}
+		greenSpaces5[6] = new GameSpace("green", 2, redSpaces5[7], null, endX-128, endY);
+		
+		GameSpace[] mergeJoinSpace = new GameSpace[11];
+	}
 	
 }
--- src/main/java/edu/asu/cst316/gameboard/GameSpace.java
+++ src/main/java/edu/asu/cst316/gameboard/GameSpace.java
@@ -1,4 +1,4 @@
-package main.java.edu.asu.cst316.gameboard;
+package edu.asu.cst316.gameboard;
 
 public class GameSpace {
 	
@@ -6,12 +6,16 @@ public class GameSpace {
 	private int size;
 	private GameSpace nextSpace = null;
 	private GameSpace alternativeSpace = null;
+	private int posX;
+	private int posY;
 	
-	public GameSpace(String type, int size, GameSpace nextSpace, GameSpace alternativeSpace){
+	public GameSpace(String type, int size, GameSpace nextSpace, GameSpace alternativeSpace, int posX, int posY){
 		this.type = type;
 		this.size = size;
 		this.nextSpace = nextSpace;
 		this.alternativeSpace = alternativeSpace;
+		this.posX = posX;
+		this.posY = posY;
 	}
 	
 	public String getType(){
