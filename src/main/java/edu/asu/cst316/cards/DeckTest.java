4fcc3052c356e4b867bdfca75ca6bbd477b8f72e
Austin Crothers <ajcrothe@asu.edu>
2015-04-16 18:25:02 -0700

    created a junit file

---
+++ src/main/java/edu/asu/cst316/cards/DeckTest.java
@@ -0,0 +1,14 @@
+package main.java.edu.asu.cst316.cards;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DeckTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
